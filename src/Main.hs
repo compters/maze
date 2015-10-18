db08e6e20eca6d7a8c047fcb42e76d2d97345f2b
Ian Compton <ian.a.compton@googlemail.com>
2015-10-18 18:14:43 +0100

    Refactored into state monad, added solver
    Maze logic is now in its own StateT transformer so generation
    algorithms can be easily integrated.
    Simple maze solver has been added

--- src/Main.hs
+++ src/Main.hs
@@ -1,125 +1,32 @@
 module Main where
 
-import Debug.Trace
-import Data.List
-import Data.Vector (Vector)
-import qualified Data.Vector as V
-import System.Random.MWC
-import Data.Map (Map)
-import qualified Data.Map as Map
+import Options.Applicative
+import Maze
 
-data C = Wall
-       | Cl (Int, Int)
-  deriving (Eq, Ord)
 
-instance Show C where
-  show Wall   = "Wall"
-  show (Cl _) = "Cell"
+data MazeOptions = MazeOptions {
+    rowCount :: Int
+  , colCount :: Int
+ } deriving (Show, Eq)
 
-data Cell = Cell { row :: !Int
-                 , col :: !Int
-                 , links :: Map (Int, Int) Bool
-                 , north :: C
-                 , south :: C
-                 , east :: C
-                 , west :: C  }
-    deriving (Ord)
 
-coords :: Cell -> (Int, Int)
-coords c = (row c, col c)
-{-# INLINE coords #-}
-
-instance Show Cell where
-  show (Cell r c _ n s e w) = "|C(" ++ show r ++ "," ++ show c ++ ") " ++ "N-" ++ show n ++ ",S-" ++ show s ++ ",E-" ++ show e ++ ",W-" ++ show w ++ "|"
-
-type Row = Vector Cell
-
-data Grid = Grid { rows :: !Int, cols :: !Int, grid :: Vector Row }
-    deriving (Eq)
-
-renderRow :: Row -> String
-renderRow cells = let (top, bottom) = foldl' toS ("", "") cells in
-  "|" ++ top ++ "\n" ++ "+" ++ bottom
-instance Show Grid where
-  show (Grid r c g) = prelude ++ "\n" ++ body
-      where
-        prelude = "+" ++ concat (replicate c "---+")
-        body = intercalate "\n" (V.toList $ V.map renderRow g)
-
-toS (top, bottom) c@(Cell _ _ _ _ s e _) = (top ++ "   " ++ boundary c e, bottom ++ sBoundary c s ++ "+")
-sBoundary c Wall = "---"
--- The C' values here are the OLD values of c', pre links!
-sBoundary c (Cl c') = if linked c' c then "   " else "---"
-boundary c Wall = "|"
-boundary c (Cl c') = if linked c' c then " " else "|"
-
-instance Eq Cell where
-  (==) c c' = (row c == row c') && (col c == col c')
-
-newCell :: Int -> Int -> Grid -> Cell
-newCell r c grid = Cell r c Map.empty (get grid n) (get grid s) (get grid e) (get grid w)
+runMazeIO :: MazeOptions -> IO ()
+runMazeIO (MazeOptions rows cols) = maze >>= print
     where
-      n = (r - 1, c)
-      s = (r + 1, c)
-      e = (r, c + 1)
-      w = (r, c - 1)
-
-get :: Grid -> (Int, Int) -> C
-get (Grid rows cols g ) a@(r, c) = if inBounds then Cl (r, c) else Wall
-  where inBounds = r >= 0 && r < rows && c >= 0 && c < cols
-
-newGrid :: Int -> Int -> Grid
-newGrid r c = gg
-    where g = V.fromList [newRow i | i <- [0 .. r -1]]
-          newRow i = V.fromList [newCell i j gg | j <- [0..c-1]]
-          gg = Grid r c g
-
-linkCells :: Grid -> Cell -> Cell -> Grid
-linkCells g a b = replace b' $ replace a' g
-    where
-      a' = a { links = Map.insert (coords b) True (links a) }
-      b' = b { links = Map.insert (coords a) True (links b) }
-
-unlinkCells :: Cell -> Cell -> (Cell, Cell)
-unlinkCells a b = (a', b')
-    where
-      a' = a { links = Map.delete (coords b) (links a) }
-      b' = b { links = Map.delete (coords a) (links b) }
-
-linked :: (Int, Int) -> Cell -> Bool
-linked a b = Map.member a (links b)
-
-lookupCell :: Grid -> (Int, Int) -> Cell
-lookupCell g (r, c) = (grid g V.! r) V.! c
-
-neighbors :: Grid -> Cell -> [Cell]
-neighbors g (Cell _ _ _ n s e w) = [lookupCell g c | Cl c <- [n, s, e, w]]
-
-eachCell :: Monad m => Grid -> (Grid -> Cell -> m Grid) -> m Grid
-eachCell g@(Grid r c rows) f = V.foldM' foldRow g rows
-  where
-    foldRow = V.foldM' f
-
-replace :: Cell -> Grid -> Grid
-replace c@(Cell row col _ _ _ _ _) g = g { grid = (grid g) V.// [(row, gRow)] }
-   where gRow = (grid g V.! row) V.// [(col, c)]
-
-binaryTree :: Int -> Int -> IO Grid
-binaryTree rows cols = do
-  let grid = newGrid rows cols
-  rand <- createSystemRandom
-  g' <- eachCell grid (walker rand)
-  return g'
-  where
-    walker :: GenIO -> Grid -> Cell -> IO Grid
-    walker rand g c@(Cell row col links n _ e _) = do
-      let candidates = [cx | Cl cx <- [n, e]]
-      if not (null candidates) then do
-        i <- uniformR (0, length candidates - 1) rand
-        return $ linkCells g (lookupCell g $ candidates !! i) c
-      else return g
+      maze = newMaze (rows, cols) 0 >>= evalMaze (binaryTree >> djikstra)
 
 main :: IO ()
-main = do
-  grid <- binaryTree 10 10
-  print grid
+main = execParser opts >>= runMazeIO
+ where
+   opts = info (helper <*> optionsParser) (fullDesc <> progDesc "Output a maze")
+
+optionsParser :: Parser MazeOptions
+optionsParser = MazeOptions
+                <$> option auto (long "rows"
+                              <> value 8 
+                              <> short 'r'
+                              <> help "How many rows to output")
+                <*> option auto (long "columns"
+                              <> value 8 
+                              <> short 'c'
+                              <> help "How many columns to output")
---
+++ src/Maze.hs
@@ -0,0 +1,273 @@
+{-# LANGUAGE TemplateHaskell, GeneralizedNewtypeDeriving, RankNTypes, OverlappingInstances, FlexibleInstances #-}
+
+module Maze where
+
+import Debug.Trace
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Control.Monad
+import Control.Lens
+import Control.Monad.State
+import Data.List
+import Data.Vector (Vector, (!), (//))
+import qualified Data.Vector as V
+import Data.Set (Set)
+import qualified Data.Set as Set
+import System.Random.MWC
+
+class (Show a) => GShow a where
+  gshow :: a -> String
+  gshow = show
+
+instance GShow String where
+  gshow = id
+
+instance GShow Char where
+  gshow c = [c]
+
+instance GShow Int where
+  gshow = show
+
+data C = Wall
+       | Cl (Int, Int)
+    deriving (Eq, Ord, Show)
+
+data Cell a = Cell {
+        _row   :: !Int
+      , _col   :: !Int
+      , _links :: Set (Int, Int)
+      , _north :: C
+      , _south :: C
+      , _east  :: C
+      , _west  :: C
+      , _val   :: a
+  }
+  deriving (Show, Eq)
+makeLenses ''Cell
+
+type Row a = Vector (Cell a)
+
+data Maze a = Maze {
+    _rowCount  :: !Int
+  , _colCount  :: !Int
+  , _grid      :: Vector (Row a)
+  , _rand      :: GenIO
+  }
+
+makeLenses ''Maze
+
+instance Functor Maze where
+  fmap f m = m & grid %~ newGrid
+      where
+        newGrid = V.map (mapRow f)
+        mapCell f c = c & val %~ f
+        mapRow f = V.map (mapCell f) 
+
+instance GShow a => Show (Maze a) where
+  show maze = header ++ body
+      where
+        header = "+" ++ concat (replicate (maze^.colCount) cellSection) ++ "\n"
+        body = intercalate "\n" (V.toList $ V.map renderRow (maze^.grid))
+
+type MZ x = StateT (Maze x)
+
+newtype MazeM x a = MazeM { eval :: MZ x IO a }
+    deriving (Functor, Applicative, Monad, MonadState (Maze x), MonadIO)
+
+cellWidth = 3
+topWall = concat $ replicate cellWidth "-"
+cellGap = concat $ replicate cellWidth " "
+cellSection = topWall ++ "+"
+
+update :: Cell x -> MazeM x ()
+update c = grid %= replaceRow
+  where
+    replaceRow g = g // [(c^.row, replaceCell (g ! (c^.row)))]
+    replaceCell r = r // [(c^.col, c)]
+
+eachCell :: r -> (r -> Cell a -> MazeM a r) -> MazeM a r
+eachCell v fn = do
+  rows <- use rowCount
+  cols <- use colCount
+  foldM runFn v [(r, c) | r <- [0 .. rows - 1], c <- [0 .. cols - 1]]
+  where
+    runFn v' crds = getCell crds >>= fn v'
+
+eachCell_ :: (Cell a -> MazeM a ()) -> MazeM a ()
+eachCell_ fn = eachCell () (\ _ c -> fn c)
+
+coords :: Cell a -> (Int, Int)
+coords c = (c^.row, c^.col)
+
+getCell :: (Int, Int) -> MazeM a (Cell a)
+getCell (r, c) = do
+  g <- use grid
+  return $ (g ! r) ! c
+
+linkCells :: Cell a -> Cell a -> MazeM a ()
+linkCells a b = do
+  update a'
+  update b'
+  where
+    a' = a & links %~ Set.insert (coords b)
+    b' = b & links %~ Set.insert (coords a)
+
+unlinkCells :: Cell a -> Cell a -> MazeM a ()
+unlinkCells a b = do
+  update a'
+  update b'
+  where
+    a' = a & links %~ Set.delete (coords b)
+    b' = b & links %~ Set.delete (coords a)
+
+neighbors :: Cell a -> MazeM a [Cell a]
+neighbors c = sequence [getCell cl | Cl cl <- [n, s, e, w]]
+    where
+      n = c^.north
+      s = c^.south
+      e = c^.east
+      w = c^.west
+
+pickRandomly :: [x] -> MazeM a x
+pickRandomly xs = do
+  r <- use rand
+  i <- liftIO $ uniformR (0, length xs - 1) r
+  return $ xs !! i
+
+newMaze :: (Int, Int) -> a -> IO (Maze a)
+newMaze (rows, cols) v = do
+  rnd <- createSystemRandom
+  return $ Maze rows cols newGrid rnd
+  where
+    newGrid     = V.fromList [newRow r | r <- [0 .. rows - 1]]
+    newRow r = V.fromList [newCell (r, c) | c <- [0 .. cols - 1]]
+    newCell (r, c) = Cell r c Set.empty n s e w v 
+      where
+        n = getC (r - 1, c)
+        s = getC (r + 1, c)
+        e = getC (r, c + 1)
+        w = getC (r, c - 1)
+    getC (r, c) =
+      if inBounds then Cl (r, c) else Wall
+      where
+        inBounds = r >= 0 && r < rows && c >= 0 && c < cols
+
+linked :: Cell a -> (Int, Int) -> Bool
+linked cell crds = Set.member crds (cell^.links) 
+
+renderRow :: GShow a => Row a -> String
+renderRow cells = let (rowTop, rowBottom) = foldl renderCell ("", "") cells in
+  "|" ++ rowTop ++ "\n" ++ "+" ++ rowBottom
+
+padContents :: GShow a => a -> String
+padContents x = pad (gshow x)
+    where
+      pad str | length str < cellWidth && length str `mod` 2 == 0 = pad $ str ++ " "
+              | length str < cellWidth && length str `mod` 2 /= 0 = pad $ " " ++ str
+              | otherwise = str
+
+renderCell :: GShow a => (String, String) -> Cell a -> (String, String)
+renderCell (rowTop, rowBottom) cell = (rowTop', rowBottom')
+    where
+      rowTop'    = rowTop ++ padContents (cell^.val) ++  eastBoundary
+      rowBottom' = rowBottom ++ southBoundary ++ "+"
+      eastBoundary = case cell^.east of
+                       Wall -> "|"
+                       Cl c -> if linked cell c then " " else "|"
+      southBoundary = case cell^.south of
+                       Wall -> topWall
+                       Cl c -> if linked cell c then cellGap else topWall
+
+binaryTree :: MazeM a ()
+binaryTree = eachCell_ walker
+    where
+      walker cell = do
+        let candidates = [c | Cl c <- [cell^.north, cell^.east]]
+        unless (null candidates) $ do
+          cell' <- pickRandomly candidates >>= getCell
+          linkCells cell cell'
+
+execMaze :: MazeM a b -> Maze a -> IO (Maze a)
+execMaze f = execStateT (eval f)
+
+evalMaze :: MazeM a b -> Maze a -> IO b
+evalMaze f = evalStateT (eval f)
+
+runMaze :: MazeM a b -> Maze a -> IO (b, Maze a)
+runMaze f = runStateT (eval f)
+
+uniformM :: (Int, Int) -> MazeM a Int
+uniformM x = do
+  r <- use rand
+  liftIO $ uniformR x r
+
+randomWestWallCoord :: MazeM a (Int, Int)
+randomWestWallCoord = do
+  rows <- use rowCount
+  row <- uniformM (0, rows - 1)
+  return (row, 0)
+
+randomCoord :: MazeM a (Int, Int)
+randomCoord = do
+  rows <- use rowCount
+  cols <- use colCount
+  row <- uniformM (0, rows - 1)
+  col <- uniformM (0, cols - 1)
+  return (row, col)
+
+distanceFill :: (Int, Int) -> MazeM Int (Int, (Int, Int))
+distanceFill start = do
+  startCell <- getCell start
+  update $ startCell & val .~ 0
+  res <- walkLinks 0 (Set.toList $ startCell^.links) (0, start)
+  traceShowM res
+  return res
+    where
+      walkLinks _ [] biggest = return biggest
+      walkLinks c cells (n, bg) = do
+        frontier <- mapM (updateCell (c + 1)) cells
+        walkLinks (c + 1) (concat frontier) (c + 1, head cells)
+      updateCell c crds = do
+        cell <- getCell crds
+        update $ cell & val .~ c
+        filterM isZero $ Set.toList $ cell^.links
+      isZero c = do
+        cell <- getCell c
+        return $ cell^.val == 0 && c /= start
+
+distanceFillRand :: MazeM Int (Int, (Int, Int))
+distanceFillRand = randomWestWallCoord >>= distanceFill
+
+mapCells :: (Cell a -> b) -> Maze a -> Maze b
+mapCells fn m = m & grid %~ newGrid
+    where
+      newGrid   = V.map updateRow
+      updateRow = V.map updateCellValue
+      updateCellValue cell = cell { _val = fn cell }
+
+djikstra :: MazeM Int (Maze Char)
+djikstra = do
+  path <- distanceFillAndWalk
+  mz <- get
+  return $ mapCells (markPath path) mz
+  where
+        distanceFillAndWalk = do
+          (_, end) <- distanceFillRand
+          endCell <- getCell end
+          walkLinks endCell [end]
+        walkLinks cell acc =
+          if cell^.val == 0 then
+            return $ Set.fromList acc
+          else do
+            (next, _) <- smallestLink cell
+            walkLinks next (coords next : acc)
+        pairValue :: (Int, Int) -> MazeM Int (Cell Int, Int)
+        pairValue coord = do
+          cell <- getCell coord
+          return (cell, cell^.val)
+        smallestLink :: Cell a -> MazeM Int (Cell Int, Int)
+        smallestLink cell = do
+          linkedCells <- mapM pairValue $ Set.toList (cell^.links)
+          return $ minimumBy pairCompare linkedCells
+        pairCompare (_, b) (_, b') = compare b b'
+        markPath path cell = if Set.member (coords cell) path then '*' else ' '
